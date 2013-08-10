8751b103919cc5b8d688b31d1170078fbbd83570
mkoohafkan <michael.koohafkan@gmail.com>
2013-08-10 11:37:46 -0700

    minor change to loop_solar.py to get solar rasters for all 365 days of the year

--- GRASS scripts/loop_solar.py
+++ GRASS scripts/loop_solar.py
@@ -2,9 +2,12 @@
 # for the specified sequence of days
 
 import grass.script as grass
+import os
+os.chdir("C:/repositories/codeRepo/sol")
 
-doy = [9, 23, 37, 51, 65, 79, 93, 107, 121, 135, 149, 161, 172, 182, 191, 205, 
-       219, 233, 247, 257, 266, 278, 289, 303, 317, 331, 344, 356, 365]
+doy = range(1, 366)
+#doy = [9, 23, 37, 51, 65, 79, 93, 107, 121, 135, 149, 161, 172, 182, 191, 205, 
+#       219, 233, 247, 257, 266, 278, 289, 303, 317, 331, 344, 356, 365]
 # i is the day of year (DOY) to use for running r.sun
 for i in doy:
 	# want to get insolation time, global radiation
