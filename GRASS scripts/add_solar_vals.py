28c4349db99dc95c3cb83982322166f4abf29a4b
mkoohafkan <michael.koohafkan@gmail.com>
2013-08-10 11:38:41 -0700

    modified add_solar_vals.py to work with either insolation or radiation data and get values for all days of the year

--- GRASS scripts/add_solar_vals.py
+++ GRASS scripts/add_solar_vals.py
@@ -4,24 +4,42 @@
 
 import grass.script as grass
 import string
+import os
+os.chdir("C:/repositories/codeRepo/sol")
 
+
+###################################
+#varname = 'global radiation'
+varname = 'insolation'
+###################################
+
+if varname == 'global radiation':
+	colname = 'RADDOY'
+	rastname = 'globrad_DOY'
+	outname = 'nodesglobrad.csv'
+elif varname == 'insolation':
+	colname = 'INSDOY'
+	rastname = 'insol_DOY'
+	outname = 'nodesinsol.csv'
+	
 # make copy of nodes point vector
 grass.run_command("g.copy", vect = "nodes@PERMANENT,nodelocs")
 
-doy = [9, 23, 37, 51, 65, 79, 93, 107, 121, 135, 149, 161, 172, 182, 191, 205, 
-       219, 233, 247, 257, 266, 278, 289, 303, 317, 331, 344, 356, 365]
-cols = string.join(['RADDOY' + str(i) for i in doy], ' DOUBLE PRECISION, ') + ' DOUBLE PRECISION'
-outcols = string.join(['RADDOY' + str(i) for i in doy], ',')
+#doy = [9, 23, 37, 51, 65, 79, 93, 107, 121, 135, 149, 161, 172, 182, 191, 205, 
+#       219, 233, 247, 257, 266, 278, 289, 303, 317, 331, 344, 356, 365]
+doy = range(1, 366)
 # i is the day of year (DOY) 
 for i in doy:
 	# add the column
 	grass.run_command("v.db.addcol", map = "nodelocs@PERMANENT", 
-	                  columns = "RADDOY" + str(i) + " DOUBLE PRECISION")
+	                  columns = colname + str(i) + " DOUBLE PRECISION")
+	# pull the radiation data for 
 	grass.run_command("v.what.rast", vect='nodelocs@PERMANENT', 
-	                  rast="globrad_DOY" + str(i) + "_16ft@PERMANENT", 
-					  col="RADDOY" + str(i) )
+	                  rast=rastname + str(i) + "_16ft@PERMANENT", 
+					  col=colname + str(i) )
 # save node data to file
 grass.run_command("v.out.ascii", input = "nodelocs@PERMANENT", fs = ',', 
-                  columns = outcols, output = 'noderad')
-# remove created file
+                  columns = string.join([colname + str(i) for i in doy], ','), 
+				  output = outname)
+# clean up
 grass.run_command("g.remove", flags="f", vect="nodelocs@PERMANENT")
